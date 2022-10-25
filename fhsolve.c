36a26bb753a9547ec8b05c90d39dbb87b8933d2b
alfille <paul.alfille@gmail.com>
2022-10-25 13:52:07 -0400

    Copmmon validation code

--- fhsolve.c
+++ fhsolve.c
@@ -135,6 +135,8 @@ void jsonOut( void ) ;
 
 #include "getOpts.c"
 #include "showBits.c"
+#include "help.c"
+#include "validate.c"
 
 int main( int argc, char **argv )
 {
@@ -196,81 +198,6 @@ int main( int argc, char **argv )
         }
     }
 }
-
-Validation_t validate( void ) {
-    Validation_t v = Val_ok ; // default
-
-    // length
-    if ( xlength < 3 ) {
-        fprintf(stderr,"Bad length will set to 5\n");
-        xlength = 5;
-        v = Val_fix ;
-    }
-    if ( xlength > MaxHoles ) {
-        fprintf(stderr,"Bad length will set to %d\n",MaxHoles);
-        xlength = MaxHoles;
-        v = Val_fix ;
-    }
-
-    // width
-    if ( ylength < 1 ) {
-        fprintf(stderr,"Bad width will set to 1\n");
-        ylength = 1;
-        v = Val_fix ;
-    }
-    if ( ylength > MaxHoles/3 ) {
-        fprintf(stderr,"Bad width will set to %d\n",MaxHoles/3);
-        ylength = MaxHoles/3 ;
-        v = Val_fix ;
-    }
-
-    // poison
-    if (poison<0) {
-        fprintf(stderr,"Bad poisoning will set to 0 days\n");
-        poison = 0;
-        v = Val_fix ;
-    }
-    if (poison>MaxPoison) {
-        fprintf(stderr,"Bad poisoning will set to %d days\n",MaxPoison);
-        poison = MaxPoison;
-        v = Val_fix ;
-    }
-
-    // visits (lower)
-    if (visits<1) {
-        fprintf(stderr,"Holes visited/day will be set to 1\n");
-        visits=1 ;
-        v = Val_fix ;
-    }
-
-    // Calculate holes
-    // total holes
-    switch( geo ) {
-        case Circle:
-        case Grid:
-            holes = xlength * ylength ;
-            break ;
-        case Triangle:
-            ylength = xlength ;
-            holes = xlength * ( xlength+1) / 2 ;
-            break ;
-    }
-    if ( holes > MaxHoles ) {
-        fprintf(stderr,"%d Holes calculated for %s -- greater than max %d\n",holes,geoName(geo),MaxHoles);
-        return Val_fail ;
-    }
-
-    // visits
-    if ( visits > holes ) {
-        fprintf(stderr, "Changing visits to be no larger than the number of holes (%d)\n",holes);
-        visits = holes ;
-        v = Val_fix ;
-    }
-
-    return v;
-}
-
-#include "help.c"
 
 char * geoName( Geometry_t g ) {
     switch (g) {
